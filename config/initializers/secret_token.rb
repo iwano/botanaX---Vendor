ae6fd3b339c91356c30c3f64c82c23b714ff8de3
Ivan Calderon <iwano@hotmail.com>
2012-05-28 12:21:55 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soytudaddy::Application.config.secret_token = '6a1f59b2d866c58d787c13d266ecf66bf3cc728e4689f69a568a3e1c756137b243ba78c60acece703a019692834d0185e8a02134ec4ebf854cf5c4db291e5ec0'
